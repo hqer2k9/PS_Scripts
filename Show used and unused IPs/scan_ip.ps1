d4248111d89c88dc67794770fb5d8653e59af0e5
hqer2k9 <hqer2k9@googlemail.com>
2016-02-15 14:16:02 +0100

    Adding ip adress scan script
    This script uses the ping method, you can of course use the method of
    the spooler service from my other script, check it out!

---
+++ Show used and unused IPs/scan_ip.ps1
@@ -0,0 +1,39 @@
+﻿
+# --------------------------------------------------------------------------------------
+# Script: scan_ip.ps1
+# Script Version: v1.0
+# Author: Ingo Bechtle
+# comments: Scans network range for used or unused ip adresses and shows time taken
+# --------------------------------------------------------------------------------------
+
+#Get-Date for timekeeping at the end of the script
+$count = 0
+$count1 = 0
+$Start = Get-Date
+$Ping = New-Object System.Net.Networkinformation.ping
+for ($i=1; $i -le 255; $i++) 
+{
+
+#Display a progress Bar according to the IP count to evade max count 100% warning
+Write-Progress -PercentComplete ((100*$i)/255) -Activity "Gathering IP's"
+
+                $Status = $Null
+                $IPAddress = "192.168.2." + $i
+                $Status = ($Ping.Send("$IPAddress", 1)).Status 
+                if ($Status -eq "Success") 
+                { $count++
+                Write-Host "$IPAddress" -ForegroundColor Green  
+                }  
+                else
+                { $count1++
+                Write-Host "$IPAddress" -ForegroundColor Red
+                } 
+} 
+$End = Get-Date
+
+Write-Host ""
+Write-Host $count "used IP's" -ForegroundColor Green
+Write-Host $count1 "unused IP's" -ForegroundColor Red
+
+#how long did it take?
+$End-$Start
