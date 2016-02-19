e73a6f66ef30f606cd467be4affb91b58ca2dcc3
hqer2k9 <hqer2k9@googlemail.com>
2016-02-19 17:19:09 +0100

    adding more scripts
    Adding more scripts as well as some snippets, enjoy

---
+++ Snipptes/Test_Connection.ps1
@@ -0,0 +1,12 @@
+﻿# --------------------------------------------------------------------------------------
+# Script: Test_Connection.ps1
+# Script Version: v1.0
+# Author: Ingo Bechtle
+# comments: Snippet for fast 1 ping requests, not realy reliable but it does it's job
+# --------------------------------------------------------------------------------------
+
+# Declaring computers
+$Computers = "Computer1", "Computer2", "Computer3", "Computer4"
+
+# 1 Ping packet
+Test-Connection -Count "1" -ComputerName $Computers | Out-host
